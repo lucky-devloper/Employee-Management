cafa5cc6076c66c92cb2497bc4420e5323e8c4b4
lucky-devloper <pritamsharmassm701@gmail.com>
2024-10-29 14:27:15 +0530

    admin username for login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -16,6 +16,10 @@ function Login({ handlelogin }) {
                 <input type="email" value={userEmail} onChange={(e) => setuserEmail(e.target.value)} required placeholder='Enter your email' className='h-[45px] w-[220px] bg-transparent border-2 border-[#408f68] outline-none px-5 rounded-full' />
                 <input type="password" value={userPassword} onChange={(e) => setuserPassword(e.target.value)} required placeholder='Enter password' className='h-[45px] w-[220px] bg-transparent border-2 border-[#408f68] outline-none px-5 rounded-full' />
                 <button type='submit' className='h-[45px] w-[220px] bg-[#408f68] outline-none px-5 rounded-full font-semibold text-xl hover:bg-transparent hover:border-2 border-[#408f68]'>Log in</button>
+                <div className='w-[100%] flex flex-col items-center'>
+                    <p>Username: admin@example.com</p>
+                    <p>Password: admin123</p>
+                </div>
             </form>
         </div>
     )
