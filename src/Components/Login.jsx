4ff4b608b36a28490f5bcb96fe3560a3d256f5ab
lucky-devloper <pritamsharmassm701@gmail.com>
2024-10-29 14:48:02 +0530

    example

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,14 +11,20 @@ function Login({ handlelogin }) {
         setuserPassword('')
     }
     return (
-        <div className='w-[300px] h-[300px] border-2 border-[#408f68] rounded-md mt-28'>
-            <form onSubmit={handleSubmit} className='w-[100%] h-[100%] flex flex-col items-center justify-center gap-5'>
+        <div className='w-[300px] h-[350px] border-2 border-[#408f68] rounded-md mt-28'>
+            <form onSubmit={handleSubmit} className='w-[100%] h-[100%] flex flex-col items-center justify-center gap-3'>
                 <input type="email" value={userEmail} onChange={(e) => setuserEmail(e.target.value)} required placeholder='Enter your email' className='h-[45px] w-[220px] bg-transparent border-2 border-[#408f68] outline-none px-5 rounded-full' />
                 <input type="password" value={userPassword} onChange={(e) => setuserPassword(e.target.value)} required placeholder='Enter password' className='h-[45px] w-[220px] bg-transparent border-2 border-[#408f68] outline-none px-5 rounded-full' />
                 <button type='submit' className='h-[45px] w-[220px] bg-[#408f68] outline-none px-5 rounded-full font-semibold text-xl hover:bg-transparent hover:border-2 border-[#408f68]'>Log in</button>
-                <div className='w-[100%] flex flex-col items-center'>
+                <div className='w-[100%] flex flex-col items-center text-[12px]'>
+                    <p>As a Admin</p>
                     <p>Username: admin@example.com</p>
                     <p>Password: admin123</p>
+                </div>
+                <div className='w-[100%] flex flex-col items-center text-[12px]'>
+                    <p>As a Employee</p>
+                    <p>Username: employee1@example.com</p>
+                    <p>Password: 123do</p>
                 </div>
             </form>
         </div>
